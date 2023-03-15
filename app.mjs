ff4f54e92d8144b1074aab891f9edc72fe4333ea
Hanh Huynh <huuhanhce2001@gmail.com>
2023-03-15 20:33:29 +0700

    none

--- app.mjs
+++ app.mjs
@@ -4,6 +4,7 @@ import cp from 'cookie-parser'
 import morgan from 'morgan'
 import * as dotenv from 'dotenv'
 import http from 'http'
+import passport from 'passport'
 import route from './route/index.mjs'
 
 dotenv.config()
@@ -15,6 +16,8 @@ app.use(bp.urlencoded({ extended: true }))
 app.use(bp.json())
 app.use(cp())
 app.use(morgan('dev'))
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/api', route)
 

06b15f55e9dd039c4c60f96223d08236fd04833e
Hanh Huynh <huuhanhce2001@gmail.com>
2023-03-31 20:31:12 +0700

    fix bug

--- route/index.mjs
+++ route/index.mjs
@@ -10,6 +10,5 @@ const route = express.Router()
 route.use('/auth', authRoute)
 route.use('/users', userRoute)
 route.use('/permissions', permissionRoute)
-route.use('/adafruit', adafruitRoute)
 
 export default route

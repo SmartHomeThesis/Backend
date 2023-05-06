bf6b53e7bd3ac274602d025b2b283f8cb04d46ae
Hanh Huynh <huuhanhce2001@gmail.com>
2023-05-06 23:38:44 +0700

    -m

--- app.mjs
+++ app.mjs
@@ -4,6 +4,7 @@ import cp from 'cookie-parser'
 import morgan from 'morgan'
 import * as dotenv from 'dotenv'
 import http from 'http'
+import schedule from 'node-schedule'
 import route from './route/index.mjs'
 import deviceService from './service/deviceService.mjs'
 
@@ -21,7 +22,9 @@ app.use('/api', route)
 
 // Auto calculate power consumption
 let cronExpress = '0 */1 * * *'
-deviceService.calculatePowerConsumption('smart-home.light')
+schedule.scheduleJob(cronExpress, async () => {
+    await deviceService.calculatePowerConsumption('smart-home.light_livingroom')
+})
 
 server.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`)
--- route/index.mjs
+++ route/index.mjs
@@ -8,6 +8,7 @@ import utilRoute from './utilRoute.mjs'
 
 const route = express.Router()
 
+route.get('/', (req, res) => { res.send("WELCOME TO OUR SYSTEM") })
 route.use('/auth', authRoute)
 route.use('/users', userRoute)
 route.use('/permissions', permissionRoute)
